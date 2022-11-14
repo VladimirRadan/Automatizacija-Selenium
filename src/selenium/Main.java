33a548cc4a97997d5304d5bb774eaac35c474286
vlada <vladavr3@gmail.com>
2022-11-14 20:33:15 +0100

    Initial commit

---
+++ src/selenium/Main.java
@@ -0,0 +1,35 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Main {
+
+
+
+    @Test
+    public void test(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://gigatron.rs/");
+        //runner testng ili junit
+    }
+
+    @Test
+    public void test2(){
+
+    }
+
+    @Test
+    public void test3(){
+
+    }
+
+    @Test
+    public void test4(){
+
+    }
+
+
+
+}
