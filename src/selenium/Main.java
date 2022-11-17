4238987d851e1d3993bbb3161af58e875178140e
vlada <vladavr3@gmail.com>
2022-11-17 22:10:22 +0100

    login form testing

--- src/selenium/Main.java
+++ src/selenium/Main.java
@@ -1,35 +1,56 @@
 package selenium;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 public class Main {
 
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
 
 
-    @Test
-    public void test(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://gigatron.rs/");
-        //runner testng ili junit
+        WebDriver driver = new ChromeDriver();// otvara browser
+        driver.manage().window().maximize();
+
+        String url = "https://the-internet.herokuapp.com/login";
+        driver.get(url);
+
+
+        WebElement username = driver.findElement(By.cssSelector("input[type='text']"));
+
+        username.sendKeys("tomsmith");
+
+        WebElement password = driver.findElement(By.cssSelector("input[name='password']"));
+        password.sendKeys("SuperSecretPassword!");
+
+        WebElement loginButton = driver.findElement(By.cssSelector(".radius"));
+        loginButton.click();
+
+        WebElement loginUserText = driver.findElement(By.xpath("//div[@id='flash']"));
+        String actualText = loginUserText.getText();
+        System.out.println("Actual text: " + actualText);
+
+        //actualText.split()
+
+        String adjustedActualText = actualText.substring(0, actualText.length()-1);
+
+        System.out.println("adjustedActualText " + adjustedActualText);
+
+        String expectedText = "You logged into a secure area!";
+
+        if (adjustedActualText.trim().equals(expectedText.trim())){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+        driver.quit();
+
     }
-
-    @Test
-    public void test2(){
-
-    }
-
-    @Test
-    public void test3(){
-
-    }
-
-    @Test
-    public void test4(){
-
-    }
-
 
 
 }
